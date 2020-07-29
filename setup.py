5e48609e2fa6d50c6601cbdf2b62431a2b01bc53
Sencer Selcuk <sencerselcuk@gmail.com>
2020-07-28 23:05:18 -0400

    use /tmp rather than /tmp/$USER or it fails at boot.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="i3-notifier",
-    version="0.7",
+    version="0.8",
     description="A notification daemon for i3",
     author="Sencer Selcuk",
     packages=["i3notifier", "tests"],
